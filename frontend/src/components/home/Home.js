012cf90fde9dfe22195020e45fe263ba0cd3fae9
gdel21 <56038834+gdel21@users.noreply.github.com>
2019-12-10 15:20:58 -0500

    home pro

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -19,8 +19,8 @@ class Home extends Component {
     })
   }
 
-  searchApi = () =>{
-    e.preventDefault
+  searchApi = (e) =>{
+    e.preventDefault()
     axios.get(`https://api.themoviedb.org/3/search/movie?api_key=4e7508386ba64e46c3202cad3c021203&language=en-US&query={this.state.search}&page=1&include_adult=false`)
     .then((res)=>{
       console.log(res)
@@ -39,7 +39,7 @@ class Home extends Component {
         <br/>
         Search movies
         <form onSubmit={this.searchApi}>
-          <input type="text" name="search" placeholder="Search" onChange={(e) =>{this.updateValues(e)}} />
+          <input type="text" name="search" placeholder="Search" onChange={this.updateValues} />
         </form>
       </div>
     );
