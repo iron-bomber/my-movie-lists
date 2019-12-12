a371d6abd4818fe6384a368122452d0abc0387b5
--replace-all <ignaciobellomo@gmail.com>
2019-12-12 15:52:20 -0500

    heroku

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
