fb354a79fe0959c76061f8d8b6285af231285629
--replace-all <ignaciobellomo@gmail.com>
2019-12-12 15:40:23 -0500

    yes

---
+++ frontend/build/precache-manifest.5a59364d2abb1446e068e592ff5f0fe4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db3969eb1b978d7e6e22bdd894499eb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "346371925e6ab17c9c5b",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "8aa79f8225c341fac015",
+    "url": "/static/css/main.c936e2e0.chunk.css"
+  },
+  {
+    "revision": "346371925e6ab17c9c5b",
+    "url": "/static/js/2.e409a89c.chunk.js"
+  },
+  {
+    "revision": "8fa5376de5281bf02126a36704ac7721",
+    "url": "/static/js/2.e409a89c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8aa79f8225c341fac015",
+    "url": "/static/js/main.c8f5e1a1.chunk.js"
+  },
+  {
+    "revision": "810b7eddbba2845a5756",
+    "url": "/static/js/runtime-main.4e64986e.js"
+  },
+  {
+    "revision": "17490a0afe58bf5f8a72f3f9e7236121",
+    "url": "/static/media/list.17490a0a.png"
+  },
+  {
+    "revision": "b492ffc52bdd558cc3b43a2f79178bcd",
+    "url": "/static/media/social.b492ffc5.png"
+  }
+]);
